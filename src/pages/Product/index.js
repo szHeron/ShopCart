ef1aeb131b928560dc57ae1c0fec4ba65173f1a4
szHeron <heronbr7@gmail.com>
2021-11-07 16:14:29 -0300

    added image

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -7,7 +7,7 @@ export default function Product({navigation, route}){
     return(
         <SafeAreaView style={styles.container}>
             <View style={styles.ProductContent}>
-                <Image style={{width: 400, height: 400}} source={{uri: route.params.item.img}}/>
+                <Image style={{width: 400, height: 300}} source={{uri: route.params.item.img}}/>
                 <Text style={{fontSize: 30}}>{route.params.item.name}</Text>
                 <Text style={{fontSize: 20, marginBottom: 10}}>{route.params.item.brand}</Text>
                 <Text style={{fontSize: 26, color: 'green'}}>Por R$ {route.params.item.price}</Text>
