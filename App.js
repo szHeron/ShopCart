52e52b2d9da1bdb09d5432098913ca2a5975d9c7
szHeron <heronbr7@gmail.com>
2021-11-07 16:19:32 -0300

    added signin and signup pages

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/pages/Home';
-import Products from './src/pages/Products'
+import Products from './src/pages/Products';
 import Product from './src/pages/Product';
 import Cart from './src/pages/Cart';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +18,8 @@ export default function App() {
             <Stack.Screen name="Products" component={Products} options={{headerShown: false}}/>
             <Stack.Screen name="Product" component={Product} options={{headerShown: false}}/>
             <Stack.Screen name="Cart" component={Cart} options={{headerShown: false}}/>
+            <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
+            <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
           </Stack.Navigator>
       </NavigationContainer>
   );
