15707960069069a9f2f114fc8747d1ef48153199
szHeron <heronbr7@gmail.com>
2021-11-07 13:52:24 -0300

    removed buttons navigate

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Title } from 'react-native-paper';
-import ButtonNavigation from '../../components/ButtonNavigation';
+import { Button, Title } from 'react-native-paper';
 import { styles } from './styles';
 
 export default function Home({navigation}){
     return(
         <SafeAreaView style={styles.container}>
             <Title style={styles.title}>Palhano Perfumaria</Title>
-            <ButtonNavigation navigation={navigation}/>
+            <Button onPress={navigation.navigate('Products')}>PRODUTOS</Button>
         </SafeAreaView>
     )
 };
